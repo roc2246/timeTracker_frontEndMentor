bc5ac378eece35f82481a2174cc8388fd8094519
Riley <riley.childs@yahoo.com>
2022-06-16 16:13:10 -0400

    Used .fetch() to retrive data

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-const card = document.querySelectorAll(".main-box, .metric");
-const metricStateChange = document.getElementsByClassName("metric-state-change")[0]
-const mainBox = card[0]
-
 //DOM Manipulation
+const card = document.querySelectorAll(".main-box, .metric");
+const metricStateChange = document.getElementsByClassName(
+  "metric-state-change"
+)[0];
+const mainBox = card[0];
 
 const displayWindowSize = () => {
+  if (mainBox.clientWidth <= 256) {
+    metricStateChange.style.width = "25%";
+  } else {
+    metricStateChange.style.width = "inherit";
+  }
+};
 
-    if(mainBox.clientWidth <= 256){
-        metricStateChange.style.width = "25%";
-    } else {
-        metricStateChange.style.width = 'inherit';
-    }
-
-}
-    
 window.addEventListener("resize", displayWindowSize);
 
 displayWindowSize();
+
+//Fetch data
+const activities = document.getElementsByClassName("activity");
+const time = document.getElementsByClassName("time--amount");
+const prevTime = document.getElementsByClassName("previous-metric--metric");
+
+fetch("../data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    Object.keys(data).forEach((activity) => {
+      activities[activity].innerHTML = data[activity].title;
+      time[activity].innerHTML = data[activity].timeframes.weekly.current;
+      prevTime[activity].innerHTML = data[activity].timeframes.weekly.previous;
+    });
+  });
